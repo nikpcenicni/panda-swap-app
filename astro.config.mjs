e89f9c9829a5e780d97fcd3120403d55446e500f
Nikholas Pcenicni <82239765+nikpcenicni@users.noreply.github.com>
2024-11-13 01:20:42 -0500

    Basic POC for UI

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    ssr: {
+      noExternal: ['sortablejs']
+    }
+  }
+});
