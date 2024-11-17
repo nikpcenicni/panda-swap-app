756eed67e18a92ca0dd4e94ef2991cf88ce3c5dc
Nikholas Pcenicni <82239765+nikpcenicni@users.noreply.github.com>
2024-11-17 03:32:02 -0500

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   integrations: [tailwind()],
+
   vite: {
     ssr: {
       noExternal: ['sortablejs', 'nanostores', 'gcode-preview']
     }
-  }
+  },
+
+  output: 'server',
+  adapter: netlify()
 });
