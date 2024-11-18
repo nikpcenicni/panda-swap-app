99c7d3dc025419dd0c0d581f8f7261add9fdf815
Nikholas Pcenicni <82239765+nikpcenicni@users.noreply.github.com>
2024-11-17 21:47:52 -0500

    remove netlify integration and move to github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import netlify from '@astrojs/netlify';
-
 export default defineConfig({
   integrations: [tailwind()],
 
@@ -12,6 +10,5 @@ export default defineConfig({
     }
   },
 
-  output: 'server',
-  adapter: netlify()
+  // output: 'server',
 });
